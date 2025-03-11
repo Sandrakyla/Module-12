31b5c7eae832da684c911e156425ea691364f986
Sandrakyla <churilin_10@mail.ru>
2025-03-11 12:40:06 +0300

    Splitting and combining FULL names

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+$example_persons_array = [
+    [
+        'fullname' => 'Иванов Иван Иванович',
+        'job' => 'tester',
+    ],
+    [
+        'fullname' => 'Степанова Наталья Степановна',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Пащенко Владимир Александрович',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Громов Александр Иванович',
+        'job' => 'fullstack-developer',
+    ],
+    [
+        'fullname' => 'Славин Семён Сергеевич',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Цой Владимир Антонович',
+        'job' => 'frontend-developer',
+    ],
+    [
+        'fullname' => 'Быстрая Юлия Сергеевна',
+        'job' => 'PR-manager',
+    ],
+    [
+        'fullname' => 'Шматко Антонина Сергеевна',
+        'job' => 'HR-manager',
+    ],
+    [
+        'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+        'job' => 'analyst',
+    ],
+    [
+        'fullname' => 'Бардо Жаклин Фёдоровна',
+        'job' => 'android-developer',
+    ],
+    [
+        'fullname' => 'Шварцнегер Арнольд Густавович',
+        'job' => 'babysitter',
+    ],
+];
+
+// Функция для объединения ФИО из частей
+function getFullnameFromParts($surname, $name, $patronomyc) {
+    return $surname . ' ' . $name . ' ' . $patronomyc;
+}
+
+// Функция для разбиения ФИО на части
+function getPartsFromFullname($fullname) {
+    $parts = explode(' ', $fullname);
+    return [
+        'surname' => $parts[0],
+        'name' => $parts[1],
+        'patronomyc' => $parts[2]
+    ];
+}
+
+foreach ($example_persons_array as $person) {
+    // Разбиваем ФИО на части
+    $parts = getPartsFromFullname($person['fullname']);
+    echo "Разбитое ФИО: ";
+    print_r($parts);
+
+    // Собираем ФИО обратно
+    $fullname = getFullnameFromParts($parts['surname'], $parts['name'], $parts['patronomyc']);
+    echo "Собранное ФИО: " . $fullname . "\n";
+}
